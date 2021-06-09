65dbef0de02511cd23ce83b0a52ebd51401b1e42
Farhan Eshrak <feshrakfreelancing01@gmail.com>
2021-06-09 11:23:09 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import AboutaApp from './Components/AboutaApp';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Hero from './Components/Hero';
+import Pricing from './Components/Pricing';
+import Statistics from './Components/Statistics';
+import Testimonials from './Components/Testimonials';
+import Tutors from './Components/Tutors';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Hero />
+      <AboutaApp />
+      <Statistics />
+      <Tutors />
+      <Pricing />
+      <Testimonials />
+      <Footer />
+    </>
   );
 }
 
